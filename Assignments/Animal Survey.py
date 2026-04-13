62d6dbcf509d2b2751077e5ca85368e0efe0668c
Macvel <839021@mystma.org>
2026-04-12 20:42:54 -0500

    animel survey

--- Assignments/Animal Survey.py
+++ Assignments/Animal Survey.py
@@ -1,3 +1,16 @@
 favorte_animel=input("What is your favorte animal:")
 habatate=input("Where do they live:")
 why_they_like=input("Why do you like them:")
+fun_fact=input("What is one fun fact about" + " " + favorte_animel+":")
+how_they_look=input("what color is " + " " + favorte_animel+":")
+
+print(" ")
+print(" ")
+
+
+#printing there responce
+print("Your favorte animel is " + " " + favorte_animel)
+print("You like them because" + " " + why_they_like)
+print("They live in" + " " + habatate)
+print("They are" + " " + how_they_look)
+print("A fun fact about them is" + " " + fun_fact)
