1e36e9006dcea6e1061f6070f9b0211710bfd2a1
Macvel <839021@mystma.org>
2026-04-12 22:36:14 -0500

    mad lib

--- Assignments/Simple Mad Lib.py
+++ Assignments/Simple Mad Lib.py
@@ -1,18 +1,27 @@
 # ask for Adjectives
-Adjective_1=input("Give me an adjective:")
-Adjective_2=input("Give me another adjective:")
-Adjective_3=input("Give me one sat adjective:")
+Adjective_1 = input("Give me an adjective: ")
+Adjective_2 = input("Give me another adjective: ")
+Adjective_3 = input("Give me one last adjective: ")
 
-#ask for Verbs
-Verb_1=input("Give me a Verb:")
-Verb_2=input("Give me another Verb:")
+# ask for Verbs
+Verb_1 = input("Give me a verb: ")
+Verb_2 = input("Give me another verb: ")
 
-#ask for Nouns
-Noun_1=input("Give me a Noun:")
-Noun_2=input("Give me another Noun:")
-Noun_3=input("Give me one me Noun:")
+# ask for Nouns
+Noun_1 = input("Give me a noun: ")
+Noun_2 = input("Give me another noun: ")
+Noun_3 = input("Give me one more noun: ")
 
-#ask forr a emotion
-emotion=input("Give me an emostion:")
+# ask for adverb
+adverb = input("Give me an adverb: ")
 
-Print("")
+# ask for emotion
+emotion = input("Give me an emotion: ")
+
+print("In a " + Adjective_1 + " world, there was a " + Adjective_2 + " " + Noun_1 +
+      " who loved to " + Verb_1 + " " + adverb +
+      ". One day, a " + Adjective_3 + " " + Noun_2 +
+      " showed up and tried to " + Verb_2 + " the " + Noun_3 +
+      ". Everyone felt very " + emotion +
+      ", and nothing was ever the same again.")
+
